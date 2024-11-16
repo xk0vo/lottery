167bec2e30aedd7bb5a80380d4553e5490a4f45b
xk0vo <187722355+xk0vo@users.noreply.github.com>
2024-11-16 17:06:53 +0800

    user auth

--- lottery_system/__init__.py
+++ lottery_system/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+import os
+# 创建users.json用于存储用户信息
+if 'users.json' not in os.listdir():
+    with open('users.json', 'w') as f:
+        f.write('{"users": []}')
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = '2024040113_3e4ad72baa24deb4ff7d093cfcace6665a83cc406cc960c9'
     from . import auth, lottery
+    # 注册蓝图
     app.register_blueprint(auth.bp)
     app.register_blueprint(lottery.bp)
     return app
--- lottery_system/auth.py
+++ lottery_system/auth.py
@@ -1,21 +1,39 @@
-from flask import Blueprint, g, request
-
+from flask import Blueprint, g, request, render_template, session, redirect
+import json
+from werkzeug.security import generate_password_hash
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
+    with open('users.json', 'r') as f:
+        users = json.load(f)
     if request.method == 'POST':
-        return ''
+        # 校验用户名
+        username, password = request.form['username'], request.form['password']
+        if username in [i['username'] for i in users['users']]:
+            return render_template('login_register.html', text='register', error_username='用户名已存在')
+        # hash密码
+        passwdhash = generate_password_hash(password)
+        users['users'].append({'username': username, 'password': passwdhash})
+        session.clear()
+        # 登录
+        session['user_id'] = username
+        # 写入用户信息
+        with open('users.json', 'w') as f:
+            json.dump(users, f)
+        return redirect('/')
     else:
-        return 'Register'
+        # 渲染注册页面
+        return render_template('login_register.html', text='register')
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         return ''
     else:
-        return 'login'
+        return render_template('login_register.html', text='login')
 
 @bp.route('/logout')
 def logout():
-    return 'logout'
+    session.clear()
+    return redirect('/')
